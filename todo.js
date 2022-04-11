4ed7c6dac5e68c86e0b4408f975b04d75131ddba
Hyerin Joo <candy980426@gmail.com>
2022-04-11 17:15:34 +0900

    ✨ localStorage에 사용자가 입력한 toDo 저장해두고, 화면에 나타내기

--- todo.js
+++ todo.js
@@ -2,6 +2,36 @@ const $toDoForm = document.getElementById("todo-form");
 const $toDoInput = document.querySelector("#todo-form input");
 const $toDoList = document.getElementById("todo-list");
 
+let savedToDo = [];
+let preToDo = JSON.parse(localStorage.getItem("toDo"));
+
+// localStoarge에 toDo 저장
+function saveToDo() {
+  savedToDo.push($toDoInput.value);
+  $toDoInput.value = "";
+  localStorage.setItem("toDo", JSON.stringify(savedToDo));
+}
+
+// 이전 toDo가 있다면 불러오기
+if (preToDo !== null) {
+  // localStorage에 추가
+  for (let i = 0; i < preToDo.length; i++) {
+    savedToDo.push(preToDo[i]);
+  }
+  // 화면 추가
+  for (let i = 0; i < savedToDo.length; i++) {
+    let li = document.createElement("li");
+    let div = document.createElement("div");
+    let btn = document.createElement("button");
+    div.innerText = savedToDo[i];
+    btn.innerText = "삭제";
+    li.appendChild(div);
+    li.appendChild(btn);
+    $toDoList.appendChild(li);
+  }
+}
+
+// form을 submit 시 발생되는 함수
 function makeList(event) {
   event.preventDefault();
   let li = document.createElement("li");
@@ -12,5 +42,6 @@ function makeList(event) {
   li.appendChild(div);
   li.appendChild(btn);
   $toDoList.appendChild(li);
+  saveToDo();
 }
 $toDoForm.addEventListener("submit", makeList);
